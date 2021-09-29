b18430f1f1c33f707c1be862e1d0411582c3eed3
Mod8124 remote <denisenriquebaca@gmail.com>
2021-09-29 11:53:16 -0600

    four

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/Vue-ui-practice/'
-      : '/'
-  }
+import { defineConfig } from "vite";
+import vue from '@vitejs/plugin-vue'
+
+//https://vitejs.dev/config/
+
+export default defineConfig({
+  base:'Vue-ui-practice',
+  plugins: [vue()]
+})
